53607f31fe6f574e7e270a9d823348cc1666c510
Kateryna Verveda <katushka211@gmail.com>
2023-03-03 13:36:21 +0100

    add part of task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.on('timeupdate', throttle(onPlayTimeUpdate, 1000));
+
+function onPlayTimeUpdate(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
