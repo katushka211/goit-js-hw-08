a85dc8e7f5774cb9e4bdb190796765f2cc6a7d07
Kateryna Verveda <katushka211@gmail.com>
2023-03-05 23:44:41 +0100

    add task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log('Отправляем форму');
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event) {
+  const message = event.currentTarget.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    console.log(savedMessage);
+  }
+}
